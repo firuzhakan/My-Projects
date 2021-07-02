bdee4d7294b2e137ecce1b7175aac6e0fae7593c
firuzhakan <firuzhakan@gmail.com>
2021-07-03 00:16:09 +0300

    3 July 21

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__)  ### object oluşturuyoruz
-@app.route('/')   ## url'ye assıgn etmek lazım flaskte decorater denıyor
-def hello():       ## kesme işareti main demek localhost 
-    return 'Hello World!'
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return "I like Flask!"
 
 @app.route('/second')
 def second():
-    return 'Bize her yer Malatya'
+    return "This is the second page"
 
 @app.route('/third/subthird')
 def third():
@@ -15,8 +16,8 @@ def third():
 
 @app.route('/forth/<string:id>')
 def forth(id):
-    return f'Id number of this page is {id}' ## burayı localhost:5000/forth/ numarayı kafadan sallıyoruz
+    return f'Id of this page is {id}'
 
-if __name__ == '__main__':
-    #app.run(debug = True) ## True modda çalısıyor. hata oldugunda hatayı geriye giderek çözüyor
-    app.run(host='0.0.0.0', port=80)  # ec2 da calıstırmak ıcın yukarsı koment oldu
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
